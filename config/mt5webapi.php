07deb6f5bfbf2d92396a84e647112a0834a8eca6
Amr Emad <aemaddin@gmail.com>
2019-03-27 04:51:10 +0200

    added config credentials and update composer

--- config/mt5webapi.php
+++ config/mt5webapi.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | MT5 Credentials
+    |--------------------------------------------------------------------------
+    |
+    | this is credentials for access to the mt5 server WebApi account
+    |
+    */
+    'mt5' => [
+        'ip' => env('MT5_SERVER_IP', '127.0.0.1'),
+        'port' => env('MT5_SERVER_PORT', 443),
+        'login' => env('MT5_SERVER_WEB_LOGIN', ''),
+        'password' => env('MT5_SERVER_WEB_PASSWORD', ''),
+    ]
 ];
